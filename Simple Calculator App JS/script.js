085017cb6d73e4cb9fe82b763b3aa7901a42d3f4
HendrikChris <79140979+HendrikChris@users.noreply.github.com>
2022-06-10 13:11:22 +0700

    Update Calculator script.js

--- Simple Calculator App JS/script.js
+++ Simple Calculator App JS/script.js
@@ -42,7 +42,7 @@ const calculate = () => {
             result = parseFloat(prevNumber) / parseFloat(currentNumber);
             break;
         default:
-			result = parseFloat(currentNumber);
+            result = parseFloat(currentNumber);
             break;
     }
     currentNumber = result;
